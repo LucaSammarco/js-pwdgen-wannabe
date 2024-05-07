738b52805a145ec0bbe42dc0ec521bb6b52c6491
Il tuo nome <la_tua_email@example.com>
2024-05-07 16:29:38 +0200

    Completed

---
+++ js/my_scritp.js
@@ -0,0 +1,13 @@
+let name = prompt ("Your name")
+let surname = prompt ("Your surname")
+let favoriteColor = prompt ("Your favorite colors")
+let num = "24"
+
+let password = name + surname + favoriteColor + num;
+
+
+
+
+
+document.getElementById("my_password").innerHTML = password;
+
